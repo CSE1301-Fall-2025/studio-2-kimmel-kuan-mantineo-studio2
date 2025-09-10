3356799e1cb4fd43050fe9a5517b00a960978d39
Mark <kimmel@wustl.edu>
2025-09-10 14:13:59 -0500

    Done

--- Ruin.java
+++ Ruin.java
@@ -3,25 +3,26 @@ public class Ruin {
     
     public static void main(String[] args){
         double startAmount = 10;
-        double winChance = 0.4;
-        double winLimit = 13;
-        
-        while (startAmount > 0 && startAmount < winLimit) {
-            if (winChance > Math.random()) {
-                startAmount++;
-                System.out.println("You won a dollar! Total = " + startAmount);
-                if (startAmount == 13) {
-                    System.out.println("It's your lucky day, you hit your win limit");
-                }                
-            } else {
-                startAmount--;
-                System.out.println("You lost a dollar! Total = " + startAmount);
-                if (startAmount == 0) {
-                    System.out.println("Go home you cause you're broke now");
+        double winChance = 0.49;
+        double winLimit = 15;
+        double bank = 500;
+        for (int index = 1; index <= 20; index++) {
+            startAmount = 10;
+            while (startAmount > 0 && startAmount < winLimit) {
+                if (winChance > Math.random()) {
+                    startAmount++;    
+                    if (startAmount == winLimit) {
+                        bank = bank + 5;
+                    }
+                } else {
+                    startAmount--;
+                    if (startAmount == 0) {
+                        bank = bank - 10;
+                    }
+                    }
                 }
+                 System.out.println("After day " + index + ", your bank total is " + bank);
             }
         }
 
-}
-    
 }
